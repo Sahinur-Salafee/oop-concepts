8c837e1a3809f89d445e9754c237ec474be30acb
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-09-08 07:58:56 +0600

    Practice Abstract Class and Method.

--- abstract.php
+++ abstract.php
@@ -1,9 +1,61 @@
 <?php
 
-abstract class Shapes {
+abstract class Shape {
 
     abstract function getArea();
     abstract function getPerimeter();
 
 }
+
+class Rectangle extends Shape {
+
+    private $base, $height;
+
+    function __construct($base,$height)
+    {
+        $this->base = $base;
+        $this->height = $height;
+    }
+
+    public function setBase($base) {
+        $this->base = $base;
+    } 
+    
+    public function setHeight($height) {
+        $this->height = $height;
+    }
+
+    function getArea(){
+        return $this->base*$this->height;
+    }
+
+    function getPerimeter()
+    {
+        
+    }
+}
+
+class Triangel extends Shape {
+
+    private $base, $height;
+
+    function __construct($base,$height)
+    {
+        $this->base = $base;
+        $this->height = $height;
+    }
+
+    function getArea(){
+        return $this->base * $this->height;
+    }
+
+    function getPerimeter()
+    {
+        
+    }
+}
+
+$shape1 = new Triangel(10,5);
+echo $shape1->getArea();
+
 
