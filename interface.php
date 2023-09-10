5063b492c47a397de423a6ac53c6acf01a8b8918
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-09-10 07:06:25 +0600

    Practice Interface

---
+++ interface.php
@@ -0,0 +1,44 @@
+<?php
+
+interface School {
+
+    public function studentData();
+    public function classTeacherData($id,$subject);
+    public function classData();
+}
+
+
+
+class LowerSecondaryStudents implements School {
+
+    function studentData()
+    {
+        echo 'Students Data';
+    }
+
+    function classTeacherData($id, $subject)
+    {
+        
+    }
+
+    function classData()
+    {
+        
+    }
+}
+
+interface classTenStudents extends School {
+    function totalHigherExaminee();
+}
+
+class higherSecondaryExaminee implements classTenStudents {
+
+    function studentData(){}
+    function classTeacherData($id,$subject){}
+    public function classData(){}
+    function totalHigherExaminee(){}
+
+}
+
+$student1 = new higherSecondaryExaminee();
+echo $student1 instanceof higherSecondaryExaminee;
