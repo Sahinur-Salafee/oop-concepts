47b41bb2e0f4d7c77b366897023ae95da8d9b702
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-09-14 06:46:26 +0600

    Practice Static Methods

--- static-method.php
+++ static-method.php
@@ -1,5 +1,17 @@
 <?php
-
 class MathCalculator {
-    
+
+    static function fibonacci($n) {
+        echo "Fibonacci series up to {$n} <br>";
+    }
+
+    function factorial($n) {
+        echo "Calculate factorial for {$n} <br>";
+    }
 }
+
+//$n1 = new MathCalculator();
+//$n1->fibonacci(10);
+//$n1->factorial(20);
+
+MathCalculator::fibonacci(27);
