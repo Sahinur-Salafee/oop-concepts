612eaddcbac7bbd474cddd236bfb04f0f2f17d42
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-09-14 07:00:27 +0600

    Static property and method pratice.

--- static-method.php
+++ static-method.php
@@ -1,17 +1,30 @@
 <?php
 class MathCalculator {
 
+    static $name;  // static property
+    private $age;
+
     static function fibonacci($n) {
+        self::$name = 'John'; // call static property
+        self::doSomething(); // call static method
         echo "Fibonacci series up to {$n} <br>";
     }
 
+    static function doSomething() {
+        echo "Do something<br>";
+    }
+
     function factorial($n) {
+        self::$name = 'deo';
+        $this->age = 30;
         echo "Calculate factorial for {$n} <br>";
     }
 }
 
-//$n1 = new MathCalculator();
+$n1 = new MathCalculator();
+$n1->factorial(20);
 //$n1->fibonacci(10);
-//$n1->factorial(20);
 
+// don't need to instantiate the new object
 MathCalculator::fibonacci(27);
+echo MathCalculator::$name;
