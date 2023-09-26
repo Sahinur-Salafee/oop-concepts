183fc7354e6db8fc75bea9e47d6cbef9327b27c9
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2023-09-26 06:49:28 +0600

    Clone a object using __clone magic Method.

--- object-cloning.php
+++ object-cloning.php
@@ -25,20 +25,24 @@ class FavColor {
         $this->color = new MyColor($color); // Call another object
     }
 
-    public function updateColor($data, $color) {
-        $this->data = $data;
+    public function updateColor($color) {
         $this->color = $color;
+    }
+
+    function __clone()
+    {
+        $this->color = clone $this->color;
     }
 }
 
 $f1 = new FavColor('Some data', 'Red');
-$f2 = clone $f1;
-
-print_r($f1);
+// print_r($f1);
 echo '<br>';
+
+$f2 = clone $f1;
 print_r($f2);
 echo '<br>';
-// $f2->setData('More data');
+$f2->updateColor('Green');
 print_r($f1);
 echo '<br>';
 print_r($f2);
